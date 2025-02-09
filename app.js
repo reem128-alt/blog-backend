3c3579b077a39c7476a15988c6acfadc9ea5e287
reem128-alt <122190881+reem128-alt@users.noreply.github.com>
2025-02-10 00:06:20 +0300

    Update app.js

--- app.js
+++ app.js
@@ -14,10 +14,11 @@ const app = express();
 dbConnect();
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://your-frontend-url.onrender.com"],
+    origin: ["http://localhost:5173", "https://your-blog-frontend.onrender.com"],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    exposedHeaders: ['set-cookie']
   })
 );
 
