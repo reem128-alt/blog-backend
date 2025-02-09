2c2fdc276ea8253e285873053bb42b3e5677ead3
reem128-alt <122190881+reem128-alt@users.noreply.github.com>
2025-02-09 22:58:43 +0300

    upload files

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+require("dotenv").config();
+const dbConnect = require("./config/dbConnect");
+const path = require("path");
+const authRoute = require("./routes/authRoute");
+const userRoute = require("./routes/userRoute");
+const postRoute = require("./routes/postRoute");
+const commentRoute = require("./routes/commentRoute");
+const cors = require("cors");
+
+const cookieParser = require("cookie-parser");
+
+const app = express();
+dbConnect();
+app.use(
+  cors({
+    origin: "http://localhost:5173", // your frontend URL
+    credentials: true,
+  })
+);
+
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use("/", express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use("/auth", authRoute);
+app.use("/users", userRoute);
+app.use("/posts", postRoute);
+app.use("/comments", commentRoute);
+
+app.listen(3000, () => {
+  console.log("server is running");
+});
