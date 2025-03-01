44b8b7973a6f7ac25e274a61ed480727b5870d0e
reem128-alt <122190881+reem128-alt@users.noreply.github.com>
2025-03-01 14:19:51 +0300

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const app = express();
 dbConnect();
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://blog-seven-sigma-81.vercel.app","https://blog-backend-myeh.onrender.com/"],
+    origin:"*",
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
