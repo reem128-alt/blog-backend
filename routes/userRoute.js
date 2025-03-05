87118e9078b2873ade92d933d5d04b2005c68106
reem128-alt <122190881+reem128-alt@users.noreply.github.com>
2025-03-06 02:56:58 +0300

    Update userRoute.js

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,12 +2,14 @@ const express = require("express")
 const router = express.Router()
 const userController = require("../controller/userController")
 const verifyJwt = require("../middleware/verifyJwt")
+const upload = require("../middleware/multer")
 
 
 router.use(verifyJwt)
 router.get("/", userController.getUsers)
 router.get("/:userId", userController.getUser)
 router.put("/update/:userId", userController.updateUser)
+router.put("/profile-picture",upload.single("profilePicture"), userController.updateUserProfile)
 router.delete("/:userId", userController.deleteUser)
 
 module.exports = router
